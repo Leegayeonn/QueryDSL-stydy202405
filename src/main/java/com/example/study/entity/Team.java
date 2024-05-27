90ec010e34224cea5cae0b9a8137cc133cc0468f
lgy <ini03134@naver.com>
2024-05-27 11:10:34 +0900

    Initial commit

---
+++ src/main/java/com/example/study/entity/Team.java
@@ -0,0 +1,30 @@
+package com.example.study.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString(exclude = "members") @EqualsAndHashCode(of = "id")
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tbl_team")
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "team_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    @Builder.Default // 내가 지정한 값을 빌더로 사용해줘라!!
+    private List<Member> members = new ArrayList<>();
+
+
+}
